1546d1366666ab085db194a0a6105c34c9279e24
Wilhuff-Tarkin <giantcaterpillar@gmail.com>
2019-08-03 11:02:29 +0200

    initial commit

---
+++ src/main/java/net/koziczak/dziennik/DziennikApplication.java
@@ -0,0 +1,13 @@
+package net.koziczak.dziennik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DziennikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DziennikApplication.class, args);
+    }
+
+}
